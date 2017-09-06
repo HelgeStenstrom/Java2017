4bad467feaa551e93d0edfa3960ef91d7eb31b73
Helge Stenström <h.stenstrom@gmail.com>
2017-09-06 18:22:13 +0200

    Lesson 01 : Java Programming Basics

--- HelloWorldHelge/src/helloworld/Main.java
+++ HelloWorldHelge/src/helloworld/Main.java
@@ -1,8 +1,15 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Vad heter du? ");
+        String yourname = input.nextLine();
+
+        System.out.printf("Hej %s!", yourname);
     }
 }
