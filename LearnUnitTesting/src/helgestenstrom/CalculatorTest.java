be018b71a656805113fc61e4840104433a5c180b
Helge Stenström <h.stenstrom@gmail.com>
2017-10-18 21:34:56 +0200

    Exempel på unit test med inmanting från tangentbord.

--- LearnUnitTesting/src/helgestenstrom/CalculatorTest.java
+++ LearnUnitTesting/src/helgestenstrom/CalculatorTest.java
@@ -1,6 +1,8 @@
 package helgestenstrom;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @org.junit.Test
@@ -9,4 +11,26 @@ public class CalculatorTest {
         int sum = c.evaluate("1+2+3");
         assertEquals(6,sum);
     }
+
+    @org.junit.Test
+    public void anotherTest() {
+        assertEquals(3, 3);
+    }
+
+    @org.junit.Test
+    public void thirdTest() {
+
+        // Setup test
+        ByteArrayInputStream in = new ByteArrayInputStream("11 12 13 14 5 My string".getBytes());
+        System.setIn(in);
+
+        // Exercise
+        int anInt = Main.testUserInput();
+        assertEquals( anInt, 5);
+
+        // tear down
+        System.setIn(System.in);
+
+    }
+
 }
--- LearnUnitTesting/src/helgestenstrom/Main.java
+++ LearnUnitTesting/src/helgestenstrom/Main.java
@@ -1,6 +1,8 @@
 package helgestenstrom;
 
- class LocalCalculator {
+import java.util.Scanner;
+
+class LocalCalculator {
     public int evaluate(String expression) {
         int sum = 0;
         for (String summand: expression.split("\\+"))
@@ -19,5 +21,18 @@ public class Main {
 
         Calculator c2 = new Calculator();
         System.out.println(c.evaluate(("100+12")));
+    }
+
+    public static int testUserInput() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Give a number between 1 and 10");
+        int input = keyboard.nextInt();
+
+        while (input < 1 || input > 10) {
+            System.out.println("Wrong number, try again.");
+            input = keyboard.nextInt();
+        }
+
+        return input;
     }
 }
