9fec3092d46e23d9bc4129cffb315fac80bc7078
Helge Stenström <h.stenstrom@gmail.com>
2017-10-29 22:29:46 +0100

    Kanske jag gör ett JavaFX GUI
    Men hur lägger man till JavaFX till ett befintligt projekt?

---
+++ ED1U3/src/helgestenstrom/D1P3B/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017. Helge Stenström
+ */
+
+package helgestenstrom.D1P3B;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
