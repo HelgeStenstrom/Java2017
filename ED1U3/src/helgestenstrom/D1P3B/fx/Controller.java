af0e0d144d28bd42b5c4b73eacc0ba500cb4a452
Helge Stenström <h.stenstrom@gmail.com>
2017-11-01 07:59:30 +0100

    Anpassar fxml-fil och fx-klasser till befintliga package.

--- ED1U3/src/helgestenstrom/D1P3B/fx/Controller.java
+++ ED1U3/src/helgestenstrom/D1P3B/fx/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package helgestenstrom.D1P3B.fx;
 
 public class Controller {
     public void clickedA() {
--- ED1U3/src/helgestenstrom/D1P3B/fx/Main.java
+++ ED1U3/src/helgestenstrom/D1P3B/fx/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package helgestenstrom.D1P3B.fx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
