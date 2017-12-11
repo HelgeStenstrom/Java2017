e34314767fae93ba8b87628c74cbbe7b5dbe8bf4
Helge Stenström <h.stenstrom@gmail.com>
2017-12-11 22:02:10 +0100

    Callbacks för alla knappar fungerar
    Men de gör inte så mycket vettigt.

--- WineCellar/src/Helge/ListController.java
+++ WineCellar/src/Helge/ListController.java
@@ -1,9 +1,30 @@
 package Helge;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ListController {
-    public void addWine(ActionEvent actionEvent) {
+    public void addWine(ActionEvent actionEvent) throws IOException {
         System.out.println("Klickade Add");
+
+        Parent root = FXMLLoader.load(getClass().getResource("mockups/WineEntryForm.fxml"));
+
+        Stage stage = new Stage();
+        stage.setTitle("Regigera nytt vin");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void editWine(ActionEvent actionEvent) {
+        System.out.println("Klickade Redigera");
+    }
+
+    public void removeWine(ActionEvent actionEvent) {
+        System.out.println("Klickade Ta bort");
     }
 }
---
+++ WineCellar/src/Helge/entryController.java
@@ -0,0 +1,13 @@
+package Helge;
+
+import javafx.event.ActionEvent;
+
+public class entryController {
+    public void okAction(ActionEvent actionEvent) {
+        System.out.println("Klickade OK");
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+        System.out.println("Klickade Avbryt");
+    }
+}
