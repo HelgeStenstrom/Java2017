dc38f46c01d5539e4f6a3cb30507aa085ef9a3a6
Helge Stenström <helge.stenstrom@ericsson.com>
2017-12-11 14:33:57 +0100

    Callback till "Lägg till vin"

--- WineCellar/src/Helge/Controller.java
+++ WineCellar/src/Helge/Controller.java
@@ -1,4 +1,9 @@
 package Helge;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void addWine(ActionEvent actionEvent) {
+        System.out.println("Klickade Add");
+    }
 }
