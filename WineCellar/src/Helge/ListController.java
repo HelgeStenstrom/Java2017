fd878361b649ca1535f02bc5b97bc65600a53579
Helge Stenström <h.stenstrom@gmail.com>
2017-12-15 17:37:00 +0100

    Nu fungerar TableView add row!

--- WineCellar/src/Helge/ListController.java
+++ WineCellar/src/Helge/ListController.java
@@ -1,28 +1,60 @@
+/*
+ * Copyright (c) 2017. Helge Stenström
+ */
+
 package Helge;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
+import javax.swing.event.TableModelListener;
 import java.io.IOException;
 
-public class ListController {
-    public ListController() {
-        // initTableView();
-    }
 
+
+public class ListController {
+
+    // Fields
+
+    public Button btnAddWine;
+    public Button btnEdit;
+    public Button btnRemove;
+    private CellarManager cellarManager = new CellarManager();
+    public TableColumn colCharacter;
+    public TableColumn colName;
+    public TableColumn colType;
+    public TableColumn colVintage;
+    public MenuBar mbAppMenu;
+    private Stage primaryStage;
+    public TableView tvWines;
+
+
+    /**
+     * The MVC(?) controller for the wine list form. It gets the primaryStage from the application starter.
+     * @param primaryStage
+     * @throws Exception
+     */
     public ListController(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         setupStage();
         initTableView();
     }
 
+    /**
+     * @return the tableview for wines.
+     */
     public TableView getTvWines() {
         return tvWines;
     }
@@ -56,11 +88,6 @@ public class ListController {
     }
 
 
-    private Stage primaryStage;
-    public Button btnAddWine;
-    public Button btnEdit;
-    public Button btnRemove;
-    public TableView tvWines;
 
     public TableColumn getColName() {
         return colName;
@@ -70,11 +97,7 @@ public class ListController {
         this.colName = colName;
     }
 
-    public TableColumn colName;
-    public TableColumn colType;
-    public TableColumn colVintage;
-    public TableColumn colCharacter;
-    private CellarManager cellarManager = new CellarManager();
+
 
     public void setupStage() throws Exception{
         // https://stackoverflow.com/questions/23132302/invocationtargetexception-when-running-a-javafx-program
@@ -91,6 +114,7 @@ public class ListController {
 
 
     public void addWine(ActionEvent actionEvent) throws IOException {
+        // TODO: Förstå varför IntelliJ inte tycker att metoden används.
         System.out.println("Klickade Add");
 
         WineBase workwine = White.exampleWhite();
@@ -106,17 +130,36 @@ public class ListController {
     }
 
     private void initTableView() {
-        TableColumn tc1 = new TableColumn("Ett");
-        TableColumn tc2 = new TableColumn("Två");
+        TableColumn newName = new TableColumn("Namn(ny)");
+        TableColumn newCharacter = new TableColumn("Karaktär(ny)");
+        TableColumn<Wine, String> charCol = new TableColumn<>("Karaktär");
+
+        TableColumn<Wine, String> nameCol = new TableColumn<>("Namn 3");
+        nameCol.setCellValueFactory(celldata -> new ReadOnlyStringWrapper(celldata.getValue().getName()));
+
+        // charCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getEthic().getStyle()));
+        // charCol.setCellValueFactory(o -> o.getValue().toString());
+        // charCol.setCellValueFactory(o -> o.getValue().getCharacterType().toString());
+
+        //newName.setCellValueFactory(o -> new ReadOnlyStringWrapper(o.getValue()));
+        //ObservableList<Wine> wines = new O
+
         ObservableList x = tvWines.getColumns();
-        tvWines.getColumns().addAll(tc1, tc2);
+        x.clear();
+        tvWines.getColumns().addAll(nameCol);
+        //tvWines.getItems().add(new String[] {"test of adding a row"});
+        Wine white =  White.exampleWhite();
+        tvWines.getItems().add(White.exampleWhite());
     }
 
     private void updateTableView() {
-        
+
+        tvWines.getItems().clear();
         for(Wine wine : cellarManager.getWines() ) {
-            System.out.println(wine.getName());
-            
+            System.out.printf("Vin från cellarManager: %s\n", wine.getName());
+            // TODO: lägg till vin i listan
+            // tvWines.getItems().add(new String[] {"hej", "hopp", "tre", "fyra"});
+            tvWines.getItems().add(wine);
         }
     }
 
